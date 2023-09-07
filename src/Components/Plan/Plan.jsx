261758321a4c9da19cea3494fbba1f778623d835
Mahmoud-Ahmed <lya440676@gmail.com>
2023-09-07 11:19:40 +0300

    new changes

--- src/Components/Plan/Plan.jsx
+++ src/Components/Plan/Plan.jsx
@@ -4,48 +4,48 @@ import "./plan.css"
 export default function Plan() {
     return (
         <div>
-            <section class="plan" id="Plan">
-                <h4 class="heading">membership plan</h4>
-                <div class="box-container">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <div class="plan-title">Basic</div>
-                            <div class="plan-sale">25$/<span>Mo</span> <br /> <span>3 Months</span> </div>
-                            <ul class="">
+            <section className="plan" id="Plan">
+                <h4 className="heading">membership plan</h4>
+                <div className="box-container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <div className="plan-title">Basic</div>
+                            <div className="plan-sale">25$/<span>Mo</span> <br /> <span>3 Months</span> </div>
+                            <ul className="">
                                 <li>Weight Lifting</li>
                                 <li>Cardio</li>
                                 <li>Yoga</li>
                                 <li>Training</li>
                                 <li>Protein Powder</li>
                             </ul>
-                            <div class="btn">check out</div>
+                            <div className="btn">check out</div>
                         </div>
 
-                        <div class="col-md-4">
-                            <div class="plan-title">standard</div>
-                            <div class="plan-sale">40$/<span>Mo</span> <br /> <span>6 Months</span> </div>
-                            <ul class="">
+                        <div className="col-md-4">
+                            <div className="plan-title">standard</div>
+                            <div className="plan-sale">40$/<span>Mo</span> <br /> <span>6 Months</span> </div>
+                            <ul className="">
                                 <li>Weight Lifting</li>
                                 <li>Cardio</li>
                                 <li>Yoga</li>
                                 <li>Training</li>
                                 <li>Protein Powder</li>
                             </ul>
-                            <div class="btn">check out</div>
+                            <div className="btn">check out</div>
                         </div>
 
 
-                        <div class="col-md-4">
-                            <div class="plan-title">premium</div>
-                            <div class="plan-sale">50$/<span>Mo</span> <br /> <span>12 Months</span> </div>
-                            <ul class="">
+                        <div className="col-md-4">
+                            <div className="plan-title">premium</div>
+                            <div className="plan-sale">50$/<span>Mo</span> <br /> <span>12 Months</span> </div>
+                            <ul className="">
                                 <li>Weight Lifting</li>
                                 <li>Cardio</li>
                                 <li>Yoga</li>
                                 <li>Training</li>
                                 <li>Protein Powder</li>
                             </ul>
-                            <div class="btn">check out</div>
+                            <div className="btn">check out</div>
                         </div>
                     </div>
                 </div>
